71971ea1ab657e54105d3715fd908ac5b0056889
Chrystine Martins <chrystine.martins@outlook.com>
2025-08-02 17:15:28 -0300

    add getAllAppointments, getAppointments, saveAppointment functions

--- repositories/appointmentRepository.js
+++ repositories/appointmentRepository.js
@@ -1,5 +1,36 @@
+import { Appointment } from '../models/appointment.js';
+
+const getAllAppointments = async () => {
+    return await Appointment.find();
+}
+
+const getAppointments = async (id) => {
+    try {
+        return await Appointment.findById(id);
+
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+const saveAppointment = async ({ date, doctorId, pacientId }) => {
+
+    try {
+
+        const prescription = new Appointment({ date, doctorId, pacientId });
+
+        return await prescription.save();
+
+    } catch (error) {
+        throw new Error(error);
+    }
+
+}
+
+
+
 const appointmentRepository = {
-    
+
 }
 
 export default appointmentRepository;
