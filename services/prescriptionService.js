6440ed418aaec76298a9310638523b30b14bc081
Chrystine Martins <chrystine.martins@outlook.com>
2025-08-03 11:00:48 -0300

    Create functions in prescriptionService

--- services/prescriptionService.js
+++ services/prescriptionService.js
@@ -1,5 +1,62 @@
-const prescriptionService = {
+import prescriptionRepository from "../repositories/prescriptionRepository.js";
+import appointmentService from "../services/appointmentService.js";
+import pacientService from "../services/pacientService.js";
+import doctorService from "../services/doctorService.js";
+import fs from 'fs';
+import PDFDocument from "pdfkit";
 
-};
+const getAllPrescriptions = async () => {
+    return await prescriptionRepository.getAllPrescriptions();
+}
+
+const getPrescription = async (id) => {
+    return await prescriptionRepository.getPrescription(id);
+}
+
+const savePrescription = async ({ date, appointmentId, medicine, dosage, instructions }) => {
+    return await prescriptionRepository.savePrescription({ date, appointmentId, medicine, dosage, instructions });
+}
+
+const updatePrescription = async (id, { date, appointmentId, medicine, dosage, instructions, file }) => {
+    return await prescriptionRepository.updatePrescription(id,
+         { date, appointmentId, medicine, dosage, instructions, file});
+}
+
+const deletePrescription = async (id) => {
+    return await prescriptionRepository.deletePrescription(id);
+}
+
+const generatePrescriptionFile = async(prescription) => {
+    const appointment = await appointmentService.getAppointment(prescription.appointmentId);
+    const pacient = await pacientService.getPacient(appointment.pacientId);
+    const doctor = await doctorService.getDoctor(appointment.doctorId);
+
+    const id = prescription._id;
+    const document = new PDFDocument({font: 'Courier'});
+    const filePath = "./prescriptions/"+ id + ".pdf";
+
+    document.pipe(fs.createWriteStream(filePath));
+    document.fontSize(16).text("Pacient Name: " + pacient.name);
+    document.fontSize(16).text("Doctor Name: " + doctor.name);
+
+    const recipe = "Medicine: " + prescription.medicine;
+    document.fontSize(12).text(recipe);
+
+    document.fontSize(12).text("Dose: " + prescription.dosage);
+    document.fontSize(12).text("Instructions: " + prescription.instructions);
+
+    document.end();
+
+    return prescription;
+}
+
+const prescriptionService = {
+    getAllPrescriptions,
+    getPrescription,
+    savePrescription,
+    updatePrescription,
+    deletePrescription,
+    generatePrescriptionFile
+}
 
 export default prescriptionService;
