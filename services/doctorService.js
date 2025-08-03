1408f8517edb0a26eba5d7d58d34a27174523a85
Chrystine Martins <chrystine.martins@outlook.com>
2025-08-03 10:46:00 -0300

    Create functions in doctorService

--- services/doctorService.js
+++ services/doctorService.js
@@ -1,5 +1,38 @@
-const doctorService = {
+import doctorRepository from '../repositories/doctorsRepository.js';
 
+const getAllDoctors = async () => {
+    return await doctorRepository.getAllDoctors();
+}
+
+const getDoctor = async (id) => {
+    return await doctorRepository.getDoctor(id);
+}
+
+const saveDoctor = async ({ name, login, password, medicalSpecialty, medicalRegistration, email, phone }) => {
+    return await doctorRepository.saveDoctor({ name, login, password, medicalSpecialty, medicalRegistration, email, phone });
+}
+
+const updateDoctor = async (id, { name, login, password, medicalSpecialty, medicalRegistration, email, phone }) => {
+    return await doctorRepository.updateDoctor(id, { name, login, password, medicalSpecialty, medicalRegistration, email, phone });
+}
+
+const deleteDoctor = async (id) => {
+    return await doctorRepository.deleteDoctor(id);
+}
+
+const getDoctorByLogin = async (login) => {
+    return await doctorRepository.getDoctorByLogin(login);
+}
+
+
+
+const doctorService = {
+    getAllDoctors,
+    getDoctor,
+    saveDoctor,
+    updateDoctor,
+    deleteDoctor,
+    getDoctorByLogin
 };
 
 export default doctorService;
