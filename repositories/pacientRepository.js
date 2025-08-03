19094679cbc3890838a17992594e9b1f1f4ff5a3
Chrystine Martins <chrystine.martins@outlook.com>
2025-08-03 10:52:34 -0300

    Create functions in pacientRepository

--- repositories/pacientRepository.js
+++ repositories/pacientRepository.js
@@ -1,5 +1,52 @@
-const pacientRepository = {
+import pacient from "../models/pacient.js";
 
+const getAllPacients = async () => {
+    try {
+        return await pacient.find();
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+const getPacient = async (id) => {
+    try {
+        return await pacient.findById(id);
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+const savePacient = async ({ name, birthDate, email, phone }) => {
+    try {
+        const pacient = new pacient({ name, birthDate, email, phone });
+        return await pacient.save();
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+const updatePacient = async (id, { name, birthDate, email, phone }) => {
+    try {
+        return await pacient.findByIdAndUpdate(id, { name, birthDate, email, phone }, { new: true });
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+const deletePacient = async (id) => {
+    try {
+        return await pacient.findByIdAndDelete(id);
+    } catch (error) {
+        throw new Error(error);
+    }
+}
+
+const pacientRepository = {
+    getAllPacients,
+    getPacient,
+    savePacient,
+    updatePacient,
+    deletePacient
 }
 
 export default pacientRepository;
