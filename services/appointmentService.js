545408cd6e94338ab74dc410ba57ac7da46f1bcf
Chrystine Martins <chrystine.martins@outlook.com>
2025-08-02 21:49:59 -0300

    add CRUD for appointmentService

--- services/appointmentService.js
+++ services/appointmentService.js
@@ -1,5 +1,33 @@
+import appointmentRepository from "../repositories/appointmentRepository";
+
+const getAllAppointments = async (id) => {
+    return await appointmentRepository.getAllAppointments(id);
+}
+
+const getAppointment = async (id) => {
+    return await appointmentRepository.getAppointment(id);
+}
+
+const saveAppointment = async ({ date, doctorId, pacientId }) => {
+    return await appointmentRepository.saveAppointment({ date, doctorId, pacientId });
+}
+
+const updateAppointment = async (id, data) => {
+    return await appointmentRepository.updateAppointment(id, data);
+}
+
+const deleteAppointment = async (id) => {
+    return await appointmentRepository.deleteAppointment(id);
+}
+
+
 const appointmentService = {
- 
+    getAllAppointments,
+    getAppointment,
+    saveAppointment,
+    updateAppointment,
+    deleteAppointment
+
 };
 
 export default appointmentService;
