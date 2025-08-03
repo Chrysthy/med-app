bce8eb7aa336538fede58c5ee421dcbf8e1d4245
Chrystine Martins <chrystine.martins@outlook.com>
2025-08-03 14:14:36 -0300

    update code

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// import { pkg, urlencoded } from 'body-parser';
+import pkg from 'body-parser';
 import express from 'express';
 import { router } from './routes/router.js';
 import db from './database/database.js'
 
 const app = express();
-// const { json, urlencoded } = pkg;
+const { json, urlencoded } = pkg;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(json());
+app.use(urlencoded({ extended: true }));
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
--- routes/appointmentController.js
+++ routes/appointmentController.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import appointmentService from '../services/appointmentService';
+import appointmentService from '../services/appointmentService.js';
 
 let router = express.Router();
 
