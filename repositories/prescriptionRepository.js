b26fc8032324b2d97f8de2e78513e644fce053b1
Chrystine Martins <chrystine.martins@outlook.com>
2025-08-03 10:56:10 -0300

    Create functions in prescriptionRepository

--- repositories/prescriptionRepository.js
+++ repositories/prescriptionRepository.js
@@ -1,5 +1,53 @@
-const prescriptionRepository = {
+import prescription from "../models/prescription.js"
 
+const getAllPrescriptions = async () => {
+    try{
+        return await prescription.find();
+    }catch(error){
+        throw new Error(error);
+    }
+}
+
+const getPrescription = async (id) => {
+    try{
+        return await prescription.findById(id);
+    }catch(error){
+        throw new Error(error);
+    }
+}
+
+const savePrescription = async ({ date, appointmentId, medicine, dosage, instructions }) => {
+    try{
+        const prescription = new prescription({ date, appointmentId, medicine, dosage, instructions });
+        return await prescription.save();
+    }catch(error){
+        throw new Error(error);
+    }
+}
+
+const updatePrescription = async (id, { date, appointmentId, medicine, dosage, instructions, file }) => {
+    try{
+        return await prescription.findByIdAndUpdate(id,
+             { date, appointmentId, medicine, dosage, instructions, file }, { new: true });
+    }catch(error){
+        throw new Error(error);
+    }
+}
+
+const deletePrescription = async (id) => {
+    try{
+        return await prescription.findByIdAndDelete(id);
+    }catch(error){
+        throw new Error(error);
+    }
+}
+ 
+const prescriptionRepository = {
+    getAllPrescriptions,
+    getPrescription,
+    savePrescription,
+    updatePrescription,
+    deletePrescription
 }
 
 export default prescriptionRepository;
