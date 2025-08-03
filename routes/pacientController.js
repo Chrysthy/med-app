aabed4c3032f23a490451c03d32d6774717257e2
Chrystine Martins <chrystine.martins@outlook.com>
2025-08-03 10:54:51 -0300

    Create router in pacientController

--- routes/pacientController.js
+++ routes/pacientController.js
@@ -1,5 +1,63 @@
-import express from 'express';
+import express from "express";
+import pacientService from "../services/pacientService.js"
 
 let router = express.Router();
+
+router.get('/pacients', async (req, res) => {
+    try {
+        const pacients = await pacientService.getAllPacients();
+        res.send(pacients);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+});
+
+router.get('/getPacient/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const pacient = await pacientService.getPacient(id);
+        res.send(pacient)
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+});
+
+router.post("/postPacient", async function (req, res) {
+    const { name, birthDate, email, phone } = req.body;
+    try {
+        const pacient = await pacientService.savePacient({ name, birthDate, email, phone });
+        res.send(pacient);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+});
+
+router.put('/pacients/:id', async (req, res) => {
+    const { id } = req.params;
+    const { name, birthDate, email, phone } = req.body;
+
+    try {
+        const pacient = await pacientService.updatePacient(id, { name, birthDate, email, phone });
+        res.send(pacient);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+});
+
+router.delete('/pacients/:id', async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        const pacient = await pacientService.deletePacient(id);
+        res.send(pacient);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send(error);
+    }
+});
 
 export default router;
