44f6bdfb1a5234b69faf8238cdb99b3ecda0118a
Chrystine Martins <chrystine.martins@outlook.com>
2025-08-09 17:23:57 -0300

    add validator with regex in pacient's models

--- models/pacient.js
+++ models/pacient.js
@@ -24,6 +24,12 @@ const pacientSchema = new Schema({
         type: String,
         required: [true, "Phone is required"],
         unique: true,
+         validate: {
+            validator: function (v) {
+                return /\d{2} 9\d{4}-\d{4}/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number! Please use the following format: XX 9XXXX-XXXX`
+        }
     },
 
     createAt: {
