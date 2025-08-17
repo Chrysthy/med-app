32284430a1dfd94f99ab604bebf5bc0176cde4c6
Chrystine Martins <chrystine.martins@outlook.com>
2025-08-17 14:12:33 -0300

    Add link to create doctor in page.tsx

--- med-app/src/app/home/page.tsx
+++ med-app/src/app/home/page.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import Link from 'next/link';
 
 export default function Home() {
     
     return (
         <>
             <h1>Welcome to the Home Page</h1>
+
+            <div>
+                <Link href="/doctor/create">Create Doctor</Link>
+            </div>
+
         </>
     );
 };
