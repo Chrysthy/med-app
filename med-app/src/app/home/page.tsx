e063329f77b8d1129365b810c677728b5964768f
Chrystine Martins <chrystine.martins@outlook.com>
2025-08-17 14:59:01 -0300

    Add Links to doctor, pacient, appointmente and prescription

--- med-app/src/app/home/page.tsx
+++ med-app/src/app/home/page.tsx
@@ -2,13 +2,26 @@ import React from 'react';
 import Link from 'next/link';
 
 export default function Home() {
-    
+
     return (
         <>
             <h1>Welcome to the Home Page</h1>
 
             <div>
-                <Link href="/doctor/create">Create Doctor</Link>
+                <Link href="/doctor/create">Create New Doctor</Link>
+
+                <br></br>
+
+                <Link href="/pacient/create">Create New Pacient</Link>
+
+                <br></br>
+
+                <Link href="/appointment/create">Create New Appointment</Link>
+
+                <br></br>
+
+                <Link href="/prescription/create">Create New Prescription</Link>
+
             </div>
 
         </>
